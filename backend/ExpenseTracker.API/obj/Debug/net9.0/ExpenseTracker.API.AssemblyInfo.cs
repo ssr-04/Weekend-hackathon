9b211cc949a70dcdac7ee845cb554308b6de63de
Sachin M Sabariram <sachinsabariram@gmail.com>
2025-06-27 21:02:19 +0530

    added gitignore

--- backend/ExpenseTracker.API/obj/Debug/net9.0/ExpenseTracker.API.AssemblyInfo.cs
+++ backend/ExpenseTracker.API/obj/Debug/net9.0/ExpenseTracker.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47d5c58de8074277fcf6fd593aa8e8d40f91f303")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da93e86ba723a230645b0db3b8e89d2cda800206")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
